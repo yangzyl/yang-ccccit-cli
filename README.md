12758fb2f95180d002810248f4b12024f8638564
liyang <15963383124@163.com>
2020-12-31 09:35:16 +0800

    init project

---
+++ README.md
@@ -0,0 +1,22 @@
+yang-ccccit-cli
+> 前端脚手架命令工具
+
+如何安装
+```
+npm install yang-ccccit-cli -g
+```
+
+
+初始化命令
+```
+yang-ccccit-cli init <projectName>
+// 选择 mobile 或者 pc 的模版
+
+或者
+yang-ccccit-cli  i <projectName>
+```
+
+版本号查看
+```
+yang-ccccit-cli -v
+```
---
+++ bin/config/index.js
@@ -0,0 +1,27 @@
+/*
+  @dest: 使用配置文件
+  @Author: yangli
+ */
+module.exports  = {
+  npmUrl: 'https://registry.npmjs.org/yang-ccccit-cli/latest',
+  promptTypeList:[{
+      type: 'list',
+      message: '请选择拉取的模版类型:',
+      name: 'type',
+      choices: [{
+        name: 'mobile',
+        value: {
+          url: 'https://github.com/yangzyl/we_ccccit_wechart.git',
+          gitName: 'vue-mobile-template',
+          val:'移动端模版'
+        }
+      },{
+        name: 'pc',
+        value: {
+          url: 'https://github.com/yangzyl/vue-cli-template.git',
+          gitName: 'vue-web-template',
+          val:'PC端模版'
+        }
+      }]
+  }],
+};
